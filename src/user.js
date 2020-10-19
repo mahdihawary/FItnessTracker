d646fd6822c12f7a0f6a9b274091313b8054d395
mahdihawary <Mahdihawary@gmail.com>
2020-10-19 18:34:03 -0400

    created initial user models

---
+++ src/user.js
@@ -0,0 +1,29 @@
+class User {
+    constructor(user) {
+        this.name = user.name
+        this.weight = user.weight
+    }
+
+    render(node) {
+        const userDiv = document.createElement("div")
+        userDiv.innerHTML = `<ul>
+        <li>${this.name}</li>
+        <li>${this.weight}</li>
+        </ul>
+        `
+        node.append(userDiv)
+    }
+    static create(node){
+        const form = document.createElement("form")
+        form.innerHTML =`
+        <label>Name</label>
+        <input name = "name">
+        <label>Weight</label> 
+        <input name ="weight">
+        <button type="submit">submit</button>
+        `
+        node.append(form)
+        
+    }
+
+}
