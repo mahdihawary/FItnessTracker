b17cc94f57ba5a9315c2c2f6346be3eb6e4b1bcc
jdhofmann <justindhofmann@gmail.com>
2020-10-20 09:27:33 -0400

    exercise class and basic fetch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", function (e) {
     const baseURL = "http://localhost:3000/api/v1/users/"
-    const main = document.querySelector("main")
+    const exerciseBaseUrl = "http://localhost:3000/api/v1/exercises/"
+
+    const content = document.querySelector("#content")
+    const aside = document.querySelector("#aside")
+
+
+    fetch(exerciseBaseUrl)
+    .then(response => response.json())
+    .then( data => {
+        for(const exercise of data){
+            let newExercise = new Exercise(exercise.name)
+            newExercise.render(aside)
+        }
+    })
+
+
 
 
 
@@ -9,9 +24,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
             const target = e.target
 
             if (target.matches("#signup")) {
-                User.create(main)
+                User.create(content)
             }
-
         })
     }
     const submitListener = () => {
@@ -29,7 +43,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
                     name: target.name.value,
                     weight: target.weight.value
                 })
-
             }
 
             fetch(baseURL, options)
@@ -39,17 +52,12 @@ document.addEventListener("DOMContentLoaded", function (e) {
         })
     }
 
-
     fetch(baseURL + 1)
         .then(response => (response.json()))
         .then(user => {
             const newUser = new User(user)
-            newUser.render(main)
+            newUser.render(content)
         })
-
-
-
-
 
     createUserEvent()
     submitListener()
