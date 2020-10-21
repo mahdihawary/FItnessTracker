b6a73dfe55ccd7dbdadb597ed9ad1648bb0d8b92
jdhofmann <justindhofmann@gmail.com>
2020-10-21 12:06:16 -0400

    refactored for render class

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", function (e) {
     const baseURL = "http://localhost:3000/api/v1/users/"
     const exerciseBaseUrl = "http://localhost:3000/api/v1/exercises/"
     const dayBaseUrl = "http://localhost:3000/api/v1/days/"
+
     const main = document.querySelector("main")
     // const content = document.querySelector("#content")
     const aside = document.querySelector("#aside")
-    
     // const formDiv = document.querySelector('.centered-form-div')
 
     const navClickListener = () => {
@@ -27,30 +27,14 @@ document.addEventListener("DOMContentLoaded", function (e) {
             }
         })
     }
-//     const navClickListener = () => {
-//         let header = document.querySelector('header');
-//         header.addEventListener('click', e => {
-//             if(e.target.matches('h1')){
-//                 console.log('render home page')
-//             }
-//             else if (e.target.matches('.userIcon')){
-//                 console.log('render user page')
-//             }
-//             else if (e.target.matches('.logoutBtn')){
-//                 console.log('logout user')
-//             }
-//             else if (e.target.matches('.statsButton')) {
-//                 getUserStats()
-//             }
-//         })
-//     }
+
     const createUserEvent = () => {
         const formDiv = document.querySelector('.centered-form-div')
         document.addEventListener('click', (e) => {
             const target = e.target
             if (target.matches("#signup")) {
-                removeLogin()
-                User.create(formDiv)
+                Render.removeLogin()
+                Render.createNewUserForm(formDiv)
             }
         })
     }
@@ -61,12 +45,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
             const target = e.target
             if (target.matches("#login")){
                 User.UserLogin(target.name.value)
-                removeFormDiv()
-                // this ^^ is async and we have to wait for an id
-                // hence this dumb timeout below
-                // setTimeout(renderUserView, 1000)
-                
-                renderUserNav()
+                Render.removeFormDiv()
+                Render.renderUserNav()
             }
             
             else if(target.matches('#newDay')){
@@ -113,10 +93,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 fetch(baseURL, options)
                     .then(response => response.json())
                     .then(user => {
-                        // console.log('newUser')
                         target.reset()
-                        removeFormDiv()
-                        renderLogin()
+                        Render.removeFormDiv()
+                        Render.renderLogin()
                     })
             }
         })
@@ -131,49 +110,49 @@ document.addEventListener("DOMContentLoaded", function (e) {
     //     }
     // })
 
-    const createFormDiv = () => {
-        const formDiv = document.createElement("div")
-        formDiv.classList.add('centered-form-div')
-        main.append(formDiv)
-    }
+    // const createFormDiv = () => {
+    //     const formDiv = document.createElement("div")
+    //     formDiv.classList.add('centered-form-div')
+    //     main.append(formDiv)
+    // }
 
-    const removeFormDiv = () => {
-        const formDiv = document.querySelector('.centered-form-div')
-        formDiv.remove()
-    }
+    // const removeFormDiv = () => {
+    //     const formDiv = document.querySelector('.centered-form-div')
+    //     formDiv.remove()
+    // }
 
-    const renderLogin = () => {
-        // should appear when page loads
-        // should disapper after user logs in/signs up
-        createFormDiv()
-        const formDiv = document.querySelector('.centered-form-div')
-        formDiv.innerHTML = `
-            <form id="login">
-                <label>UserName</label> 
-                <input name = "name">
-                <button type = "submit"> login </button>
-                <button id="signup"> signup </button>
-            </form>
-        `
-    }
+    // const renderLogin = () => {
+    //     // should appear when page loads
+    //     // should disapper after user logs in/signs up
+    //     createFormDiv()
+    //     const formDiv = document.querySelector('.centered-form-div')
+    //     formDiv.innerHTML = `
+    //         <form id="login">
+    //             <label>UserName</label> 
+    //             <input name = "name">
+    //             <button type = "submit"> login </button>
+    //             <button id="signup"> signup </button>
+    //         </form>
+    //     `
+    // }
 
-    const removeLogin = () => {
-        const formDiv = document.querySelector('.centered-form-div')
-        formDiv.innerHTML = ``;
-    }
+    // const removeLogin = () => {
+    //     const formDiv = document.querySelector('.centered-form-div')
+    //     formDiv.innerHTML = ``;
+    // }
     
     const logout =() =>{
         main.dataset.userId = "nil"
-        clearUserNav()
-        removeUserView()
-        renderLogin()
+        Render.clearUserNav()
+        Render.removeUserView()
+        Render.renderLogin()
     }
 
     
 
-    const removeUserView = () => {
-        main.innerHTML = ``
-    }
+    // const removeUserView = () => {
+    //     main.innerHTML = ``
+    // }
 
     // const renderUserView = () => {
     //     renderAsideAndContentDiv()
@@ -196,22 +175,22 @@ document.addEventListener("DOMContentLoaded", function (e) {
         //     newUser.render(content)
         // })
 
-    const renderUserNav = () => {
-        let userIcon = document.createElement("p");
-        userIcon.classList.add('userIcon');
-        userIcon.textContent = `User-Name Here`
-        let logoutBtn = document.createElement("button");
-        logoutBtn.classList.add('logoutBtn');
-        logoutBtn.textContent = `Logout`
-        let statsButton = document.createElement("button");
-        statsButton.classList.add('statsButton');
-        statsButton.textContent = `Stats`
-        document.querySelector('#userNav').append(userIcon, logoutBtn, statsButton)
-    }
+    // const renderUserNav = () => {
+    //     let userIcon = document.createElement("p");
+    //     userIcon.classList.add('userIcon');
+    //     userIcon.textContent = `User-Name Here`
+    //     let logoutBtn = document.createElement("button");
+    //     logoutBtn.classList.add('logoutBtn');
+    //     logoutBtn.textContent = `Logout`
+    //     let statsButton = document.createElement("button");
+    //     statsButton.classList.add('statsButton');
+    //     statsButton.textContent = `Stats`
+    //     document.querySelector('#userNav').append(userIcon, logoutBtn, statsButton)
+    // }
 
-    const clearUserNav = () => {
-        document.querySelector('#userNav').innerHTML = '';
-    }
+    // const clearUserNav = () => {
+    //     document.querySelector('#userNav').innerHTML = '';
+    // }
 
     const getUserStats = () =>{
         const userId = document.querySelector("main").dataset.userId
@@ -225,7 +204,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
     navClickListener()
     // renderUserNav()
-    renderLogin()
+    // renderLogin()
+    Render.renderLogin()
     createUserEvent()
     submitListener()
 })
